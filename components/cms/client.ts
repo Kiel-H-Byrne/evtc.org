6a942a3d4f1a279bce97a8bbd2751273f46a733f
kiel-h-byrne <kiel.byrne@gmail.com>
2026-05-02 23:34:09 -0400

    environment variable rename

--- components/cms/client.ts
+++ components/cms/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from "@sanity/client";
 
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_DATASET,
   token: process.env.NEXT_PUBLIC_SANITY_READ_TOKEN,
   apiVersion: "2024-01-01",
   useCdn: true,
