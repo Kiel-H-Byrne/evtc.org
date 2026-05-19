08c14117bd384dca365463455f2356f40e7fbd0a
kiel-h-byrne <kiel.byrne@gmail.com>
2026-05-19 18:26:47 -0400

    cdn

--- components/cms/client.ts
+++ components/cms/client.ts
@@ -7,5 +7,5 @@ export const sanityClient = createClient({
   dataset,
   token: process.env.NEXT_PUBLIC_SANITY_READ_TOKEN,
   apiVersion: "2024-01-01",
-  useCdn: true,
+  useCdn: false, // Set to false to bypass CDN for preview/fresh data
 });
